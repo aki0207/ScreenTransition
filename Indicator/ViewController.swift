774838d4f6f89b3ae92e9f9d597700daec77f1e7
aki0207 <forever.look.aki@docomo.ne.jp>
2019-03-03 11:29:52 +0900

    画面遷移

--- Indicator/ViewController.swift
+++ Indicator/ViewController.swift
@@ -13,8 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    @IBAction func buttonTapped(sender : AnyObject) {
+        performSegue(withIdentifier: "toViewController2",sender: nil)
+    }
+    
+   
+    //segue準備
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "toViewController2") {
+            let vc2: ViewController2 = (segue.destination as? ViewController2)!
+            //ViewControllerのtextVC2にメッセージを設定
+            vc2.textVC2 = "toVC2"
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
     }
 
+   
 
 }
 
---
+++ Indicator/ViewController2.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController2.swift
+//  Indicator
+//
+//  Created by ききょ on 2019/03/03.
+//  Copyright © 2019 kikyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    @IBOutlet var label : UILabel!
+    var textVC2:String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = textVC2
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+   
+}
